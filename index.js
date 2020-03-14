676f3a460ffdaa0d797c00a8b9c6b36b56fe9785
Iris Jiang <littleaurel@iriss-macbook.mynet>
2020-03-14 18:55:28 +0000

    Added labels

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const iris = document.querySelector("#iris");
-const luke = document.querySelector("#luke");
-const reset = document.querySelector("#reset");
+const iris_button = document.querySelector("#iris");
+const luke_button = document.querySelector("#luke");
+
+const iris_label = document.querySelector("#iris h1");
+const luke_label = document.querySelector("#luke h1");
+
+const reset_button = document.querySelector("#reset");
 
 let iris_initial_score = 15;
 let luke_initial_score = 12;
@@ -8,27 +12,27 @@ let luke_initial_score = 12;
 let iris_score = localStorage.getItem("iris_score") || iris_initial_score;
 let luke_score = localStorage.getItem("luke_score") || luke_initial_score;
 
-iris.innerText = "Iris " + iris_score;
-luke.innerText = "Luke " + luke_score;
+iris_label.innerText = iris_score;
+luke_label.innerText = luke_score;
 
-iris.onclick = () => {
+iris_button.onclick = () => {
   iris_score = parseInt(iris_score) + 1;
   localStorage.setItem("iris_score", iris_score);
-  iris.innerText = "Iris " + iris_score;
+  iris_label.innerText = iris_score;
 };
 
-luke.onclick = () => {
+luke_button.onclick = () => {
   luke_score = parseInt(luke_score) + 1;
   localStorage.setItem("luke_score", luke_score);
-  luke.innerText = "Luke " + luke_score;
+  luke_label.innerText = luke_score;
 };
 
-reset.onclick = () => {
+reset_button.onclick = () => {
   iris_score = iris_initial_score;
   localStorage.setItem("iris_score", iris_score);
-  iris.innerText = "Iris " + iris_score;
+  iris_label.innerText = iris_score;
 
   luke_score = luke_initial_score;
   localStorage.setItem("luke_score", luke_score);
-  luke.innerText = "Luke " + luke_score;
+  luke_label.innerText = luke_score;
 };
